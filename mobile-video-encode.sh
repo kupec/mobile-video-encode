a39ce7d63bfb324441cb5de8b5c484c5418f5742
kupec <kupec-k@ya.ru>
2018-07-16 22:26:01 +0300

    Force framerate

--- mobile-video-encode.sh
+++ mobile-video-encode.sh
@@ -4,4 +4,4 @@ INPUT="$1"
 OUTPUT="coded_${INPUT}.mp4"
 LOGFILE=/tmp/mobile-video-encode.log
 
-ffmpeg -i "$INPUT" -movflags +faststart -c:v libx264 -profile:v main -level 3.1 -c:a aac "$OUTPUT" &> $LOGFILE
+ffmpeg -i "$INPUT" -movflags +faststart -c:v libx264 -profile:v main -level 3.1 -c:a aac -r 25 "$OUTPUT" &> $LOGFILE
