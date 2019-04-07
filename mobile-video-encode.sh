2f00451b6c623a34e5c03baa41cba931a9440356
kupec <kupec-k@ya.ru>
2019-04-07 21:16:17 +0300

    Add yuv420p

--- mobile-video-encode.sh
+++ mobile-video-encode.sh
@@ -4,4 +4,4 @@ INPUT="$1"
 OUTPUT="coded_${INPUT}.mp4"
 LOGFILE=/tmp/mobile-video-encode.log
 
-ffmpeg -i "$INPUT" -map 0:v:0 -map 0:a:0 -movflags +faststart -c:v libx264 -profile:v main -level 3.1 -c:a aac -r 25 "$OUTPUT" &> $LOGFILE
+ffmpeg -i "$INPUT" -map 0:v:0 -map 0:a:0 -movflags +faststart -c:v libx264 -profile:v main -level 3.1 -pix_fmt yuv420p -c:a aac -r 25 "$OUTPUT" &> $LOGFILE
